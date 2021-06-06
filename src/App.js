907fc4f1854e23d5af45c434dc6c6f9ece4e9147
TOR968 <tor968@yahoo.com>
2021-06-06 21:41:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Navigation from './components/Navigation';
 import OrderClean from './components/OrderClean';
 import Аdvantages from './components/Аdvantages';
 import HowWeClean from './components/HowWeClean';
+import 'aos/dist/aos.css';
 
 function App() {
   return (
     <div className="container">
       <Navigation />
-      <OrderClean />
-      <Аdvantages />
+      <section>
+        <OrderClean />
+        <Аdvantages />
+      </section>
       <HowWeClean />
     </div>
   );
