cbb30f1980fad0b4d847181776849259c12cc2fd
TOR968 <tor968@yahoo.com>
2021-06-06 22:26:58 +0300

    Create Example.jsx

---
+++ src/components/Example.jsx
@@ -0,0 +1,26 @@
+import kitchenBG from '../photo/kitchenBG.jpg';
+import BG from '../photo/BG.jpg';
+export default function Example() {
+  return (
+    <section className="example">
+      <div className="menu">
+        <h3>Как мы убираем</h3>
+        <ul className="list-room">
+          <li>Кухня</li>
+          <li>Комнаты</li>
+          <li>Ванная</li>
+          <li>Прихожая</li>
+        </ul>
+      </div>
+
+      <ul className="photo-room">
+        <li>
+          <img src={kitchenBG} alt="kitchen" />
+        </li>
+        <li>
+          <img src={BG} alt="bath" />
+        </li>
+      </ul>
+    </section>
+  );
+}
