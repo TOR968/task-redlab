d95368fd2ade72a1fae3d951baf0dc228e290398
TOR968 <tor968@yahoo.com>
2021-06-06 22:26:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navigation from './components/Navigation';
 import OrderClean from './components/OrderClean';
 import Аdvantages from './components/Аdvantages';
 import HowWeClean from './components/HowWeClean';
+import Example from './components/Example';
 import 'aos/dist/aos.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Аdvantages />
       </section>
       <HowWeClean />
+      <Example />
     </div>
   );
 }
