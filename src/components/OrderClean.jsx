9ddc5d2e2e7c4ed51b7ac737892190f559506611
Maxim <Tor968@yahoo.com>
2021-06-08 17:17:02 +0300

    Update OrderClean.jsx

--- src/components/OrderClean.jsx
+++ src/components/OrderClean.jsx
@@ -1,8 +1,31 @@
+import { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import title from '../photo/title-bg.jpg';
 export default function OrderClean() {
+  gsap.registerPlugin(ScrollTrigger);
+  const ref = useRef(null);
+  useEffect(() => {
+    const element = ref.current;
+    gsap.to(element.querySelector('.order-btn'), {
+      duration: 3,
+      // y: element.querySelector('.example'),
+      scale: 0.8,
+      ease: 'none',
+      restart: true,
+      delay: 1,
+      scrollTrigger: {
+        trigger: element.querySelector('.order-btn'),
+        // markers: true,
+        start: 'top center',
+        // end: element.querySelector('.example'), // end after scrolling 500px beyond the start
+      },
+    });
+  }, []);
   return (
-    <div className="order">
-      <img src={title} alt="clean up" />
+    <div className="order" ref={ref}>
+      <img className="backgraund-img" src={title} alt="clean up" />
       <div className="title-text">
         <h1>Уборка квартир в Киеве </h1>
         <p>
@@ -11,7 +34,7 @@ export default function OrderClean() {
         </p>
       </div>
       <div className="order-btn">
-        <span>
+        <span className="order-text">
           Заказать <br /> уборку
         </span>
       </div>
