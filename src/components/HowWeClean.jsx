144ab0fa3c4aa22c377223869f5e490290d5a3ca
Maxim <Tor968@yahoo.com>
2021-06-11 17:25:09 +0300

    current

--- src/components/HowWeClean.jsx
+++ src/components/HowWeClean.jsx
@@ -5,7 +5,14 @@ import kitchenBG from '../photo/kitchenBG.jpg';
 import plus from '../photo/Plus.svg';
 import crossImg from '../photo/Cross.svg';
 export default function HowWeClean() {
-  const [cross, setCross] = useState(false);
+  const [cross1, setCross1] = useState(false);
+  const [cross2, setCross2] = useState(false);
+  const [cross3, setCross3] = useState(false);
+  const [cross4, setCross4] = useState(false);
+  const [cross5, setCross5] = useState(false);
+  const [cross6, setCross6] = useState(false);
+  const [cross7, setCross7] = useState(false);
+
   return (
     <>
       <section className="how-clean">
@@ -18,62 +25,173 @@ export default function HowWeClean() {
         </div>
         <div className="kitchen-box">
           <img className="kitchen" src={kitchenBG} alt="kitchen" />
-          <div className="circle" onClick={() => setCross(!cross)}>
-            <div className="circle-1">
-              <div className="circle-baner">
-                <span>Чистим фасад вытяжки</span>
-              </div>
-              <div className="circle-plus">
+
+          <div
+            className="circle-1 circle"
+            style={
+              !cross1
+                ? {
+                    'background-color': 'rgba(90, 48, 240, 0.2)',
+                  }
+                : {
+                    'background-color': 'rgba(90, 48, 240, 0)',
+                  }
+            }
+            onClick={() => setCross1(!cross1)}
+          >
+            <div className={cross1 ? 'circle-baner' : 'off'}>
+              <span>Чистим фасад вытяжки</span>
+            </div>
+            <div className="circle-plus">
+              {!cross1 ? (
                 <img src={plus} alt="plus" />
-              </div>
+              ) : (
+                <img src={crossImg} alt="cross" />
+              )}
             </div>
           </div>
-          <div className="circle-2 circle">
-            <div className="circle-baner">
+          <div
+            className="circle-2 circle"
+            style={
+              !cross2
+                ? {
+                    'background-color': 'rgba(90, 48, 240, 0.2)',
+                  }
+                : {
+                    'background-color': 'rgba(90, 48, 240, 0)',
+                  }
+            }
+            onClick={() => setCross2(!cross2)}
+          >
+            <div className={cross2 ? 'circle-baner' : 'off'}>
               <span>Моем фасады кухонных гарнитуров</span>
             </div>
             <div className="circle-plus">
-              <img src={plus} alt="plus" />
+              {!cross2 ? (
+                <img src={plus} alt="plus" />
+              ) : (
+                <img src={crossImg} alt="cross" />
+              )}
             </div>
           </div>
-          <div className="circle-3 circle">
-            <div className="circle-baner">
+          <div
+            className="circle-3 circle"
+            style={
+              !cross3
+                ? {
+                    'background-color': 'rgba(90, 48, 240, 0.2)',
+                  }
+                : {
+                    'background-color': 'rgba(90, 48, 240, 0)',
+                  }
+            }
+            onClick={() => setCross3(!cross3)}
+          >
+            <div className={cross3 ? 'circle-baner' : 'off'}>
               <span>Чистим плиту</span>
             </div>
             <div className="circle-plus">
-              <img src={plus} alt="plus" />
+              {!cross3 ? (
+                <img src={plus} alt="plus" />
+              ) : (
+                <img src={crossImg} alt="cross" />
+              )}
             </div>
           </div>
-          <div className="circle-4 circle">
-            <div className="circle-baner">
+          <div
+            className="circle-4 circle"
+            style={
+              !cross4
+                ? {
+                    'background-color': 'rgba(90, 48, 240, 0.2)',
+                  }
+                : {
+                    'background-color': 'rgba(90, 48, 240, 0)',
+                  }
+            }
+            onClick={() => setCross4(!cross4)}
+          >
+            <div className={cross4 ? 'circle-baner' : 'off'}>
               <span>Виносим мусор</span>
             </div>
             <div className="circle-plus">
-              <img src={plus} alt="plus" />
+              {!cross4 ? (
+                <img src={plus} alt="plus" />
+              ) : (
+                <img src={crossImg} alt="cross" />
+              )}
             </div>
           </div>
-          <div className="circle-5 circle">
-            <div className="circle-baner">
+          <div
+            className="circle-5 circle"
+            style={
+              !cross5
+                ? {
+                    'background-color': 'rgba(90, 48, 240, 0.2)',
+                  }
+                : {
+                    'background-color': 'rgba(90, 48, 240, 0)',
+                  }
+            }
+            onClick={() => setCross5(!cross5)}
+          >
+            <div className={cross5 ? 'circle-baner' : 'off'}>
               <span>Моем посуду</span>
             </div>
             <div className="circle-plus">
-              <img src={plus} alt="plus" />
+              {!cross5 ? (
+                <img src={plus} alt="plus" />
+              ) : (
+                <img src={crossImg} alt="cross" />
+              )}
             </div>
           </div>
-          <div className="circle-6 circle">
-            <div className="circle-baner">
+          <div
+            className="circle-6 circle"
+            style={
+              !cross6
+                ? {
+                    'background-color': 'rgba(90, 48, 240, 0.2)',
+                  }
+                : {
+                    'background-color': 'rgba(90, 48, 240, 0)',
+                  }
+            }
+            onClick={() => setCross6(!cross6)}
+          >
+            <div className={cross6 ? 'circle-baner' : 'off'}>
               <span>Протираем столешницу</span>
             </div>
             <div className="circle-plus">
-              <img src={plus} alt="plus" />
+              {!cross6 ? (
+                <img src={plus} alt="plus" />
+              ) : (
+                <img src={crossImg} alt="cross" />
+              )}
             </div>
           </div>
-          <div className="circle-7 circle">
-            <div className="circle-baner">
+          <div
+            className="circle-7 circle"
+            style={
+              !cross7
+                ? {
+                    'background-color': 'rgba(90, 48, 240, 0.2)',
+                  }
+                : {
+                    'background-color': 'rgba(90, 48, 240, 0)',
+                  }
+            }
+            onClick={() => setCross7(!cross7)}
+          >
+            <div className={cross7 ? 'circle-baner' : 'off'}>
               <span>Моем пол</span>
             </div>
             <div className="circle-plus">
-              <img src={plus} alt="plus" />
+              {!cross7 ? (
+                <img src={plus} alt="plus" />
+              ) : (
+                <img src={crossImg} alt="cross" />
+              )}
             </div>
           </div>
         </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,25 +1,31 @@
+import { useState } from 'react';
+
 import logo from '../photo/Logo.svg';
 import menu from '../photo/menu.svg';
 export default function Navigation() {
+  const [menuActive, setMenuActive] = useState(false);
   return (
     <header className="nav ">
       <img src={logo} className="logo" alt="logo" />
-      <div className="togleClean">
-        <p className="home">Дом</p>
-        <p className="office">Офис</p>
+      <nav className={menuActive ? 'nav-menu active' : 'nav-menu'}>
+        <div className="togleClean">
+          <p className="home">Дом</p>
+          <p className="office">Офис</p>
+        </div>
+        <ul>
+          <li>После ремонта</li>
+          <li>Генеральная уборка</li>
+          <li>Регулярная уборка</li>
+          <li>Мойка окон</li>
+        </ul>
+        <div className="info">
+          <span className="phone">+380 67 401 69 77</span>
+          <span>24/7</span>
+        </div>
+      </nav>
+      <div className="menu-btn" onClick={() => setMenuActive(!menuActive)}>
+        <img src={menu} className="menu" alt="menu" />
       </div>
-      <ul>
-        <li>После ремонта</li>
-        <li>Генеральная уборка</li>
-        <li>Регулярная уборка</li>
-        <li>Мойка окон</li>
-      </ul>
-      <div>
-        {/* <a href="tel:+380674016977">+380 67 401 69 77</a> */}
-        <span>+380 67 401 69 77</span>
-        <span>24/7</span>
-      </div>
-      <img src={menu} className="menu" alt="menu" />
     </header>
   );
 }
